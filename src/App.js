315d47a61023775370732cca386c88277ca92b6c
Elaman Imashov <1479020+elaman@users.noreply.github.com>
2023-03-24 00:19:19 +0600

    Add a button and create by-weekly data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import Papa from "papaparse";
+import { useRef } from "react";
 import './App.css';
 
 function App() {
-  const changeHandler = (event) => {
-    if (event.target.files.length > 0) {
-      Papa.parse(event.target.files[0], {
+  const inputRef = useRef(null);
+
+  const onParse = () => {
+    if (inputRef.current.files.length > 0) {
+      Papa.parse(inputRef.current.files[0], {
         header: true,
         skipEmptyLines: true,
         complete: function (results) {
-          console.log(results.data)
+          const sortedData = results.data.sort((a, b) => new Date(a.Date) - new Date(b.Date));
+          const startDate = new Date(sortedData[0].Date);
+          const endDate = new Date();
+
+          const weeklyData = {};
+
+          while (startDate <= endDate) {
+            const year = startDate.getFullYear();
+            const week = Math.ceil(((startDate - new Date(year, 0, 1)) / 86400000 + 1) / 7);
+
+            if (!weeklyData[year]) {
+              weeklyData[year] = {};
+            }
+
+            if (!weeklyData[year][week]) {
+              weeklyData[year][week] = { week: `W${week}-Y${year}`, amount: 0 };
+            }
+
+            startDate.setDate(startDate.getDate() + 7);
+          }
+
+          console.log(weeklyData);
+
+          sortedData.forEach(({ date, amount }) => {
+            const d = new Date(date);
+            const year = d.getFullYear();
+            const week = Math.ceil(((d - new Date(year, 0, 1)) / 86400000 + 1) / 7);
+
+            if (weeklyData[year] && weeklyData[year][week]) {
+              weeklyData[year][week].amount += amount;
+            }
+          });
+
+          const data = Object.values(weeklyData).flatMap((yearData) => Object.values(yearData));
+
+          console.log(data);
         },
       });
     };
@@ -16,12 +54,13 @@ function App() {
   return (
     <div className="App">
       <input
+        ref={inputRef}
         type="file"
         name="file"
         accept=".csv"
-        onChange={changeHandler}
         style={{ display: "block", margin: "10px auto" }}
       />
+      <button onClick={onParse}>Parse</button>
     </div>
   );
 }
